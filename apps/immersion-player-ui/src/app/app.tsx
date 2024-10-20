1a4baf9d1dfdf487565d44a6928bb941e269ef07
Patrick Elfert <patrickelfert5@gmail.com>
2024-10-20 18:02:29 +0200

    rename feature library

--- apps/immersion-player-ui/src/app/app.tsx
+++ apps/immersion-player-ui/src/app/app.tsx
@@ -1,22 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-import { FeatureContentLibrary } from '@immersion-player/feature-content-library';
-import Menu from "./menu/menu";
-import {Navbar} from "./navbar/navbar";
+import { FeatureContentLibrary } from '@immersion-player/feature-content-library-ui';
+import Menu from './menu/menu';
+import { Navbar } from './navbar/navbar';
 
 export function App() {
-
-  return <div className="grid grid-cols-root h-screen bg-plum">
-    <Menu/>
-    <div className="grid grid-rows-content h-screen">
-      <Navbar/>
-      <div className="shadow-inner-glow h-full min-h-0">
-        <div className="py-6 px-8 h-full">
-          <FeatureContentLibrary/>
+  return (
+    <div className="grid grid-cols-root h-screen bg-plum">
+      <Menu />
+      <div className="grid grid-rows-content h-screen">
+        <Navbar />
+        <div className="shadow-inner-glow h-full min-h-0">
+          <div className="py-6 px-8 h-full">
+            <FeatureContentLibrary />
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
--- libs/feature-content-library/README.md
+++
@@ -1,7 +0,0 @@
-# feature-content-library
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test feature-content-library` to execute the unit tests via [Jest](https://jestjs.io).
