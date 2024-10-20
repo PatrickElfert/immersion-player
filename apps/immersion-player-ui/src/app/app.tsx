5a5f0adb0f71daeaf5b3910a7dd148fb88ee6fd2
Patrick Elfert <patrickelfert5@gmail.com>
2024-10-20 16:12:59 +0200

    update app styles

--- apps/immersion-player-ui/src/app/app.tsx
+++ apps/immersion-player-ui/src/app/app.tsx
@@ -6,11 +6,15 @@ import {Navbar} from "./navbar/navbar";
 
 export function App() {
 
-  return <div className="grid grid-cols-root h-screen">
+  return <div className="grid grid-cols-root h-screen bg-plum">
     <Menu/>
     <div className="grid grid-rows-content h-screen">
       <Navbar/>
-      <FeatureContentLibrary/>
+      <div className="shadow-inner-glow h-full min-h-0">
+        <div className="py-6 px-8 h-full">
+          <FeatureContentLibrary/>
+        </div>
+      </div>
     </div>
   </div>;
 }
