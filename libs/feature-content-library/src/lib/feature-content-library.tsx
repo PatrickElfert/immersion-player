6e37565d56b5ec0fcd49b2a959065b6a999cba1f
Patrick Elfert <patrickelfert5@gmail.com>
2024-10-20 16:12:40 +0200

    add poc for content lib feature

--- libs/feature-content-library/src/lib/feature-content-library.tsx
+++ libs/feature-content-library/src/lib/feature-content-library.tsx
@@ -1,10 +1,26 @@
-
 /* eslint-disable-next-line */
 
+function MediaCard() {
+  return (
+    <div className="relative h-[345px] w-[300px] bg-primary bg-[url('assets/shirokumacover.png')] bg-cover flex items-end rounded">
+      <div className="absolute bg-black h-full w-full opacity-20"></div>
+      <label className="font-semibold text-2xl text-white p-3 z-10 relative">
+        Shirokuma Cafe
+      </label>
+    </div>
+  );
+}
 
 export function FeatureContentLibrary() {
   return (
-    <div>FeatureContentLibrary</div>
+    <div className="h-full flex flex-row flex-wrap gap-4 min-h-0 overflow-auto">
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+    </div>
   );
 }
 
