d0282ac09af58b626eaf18220d6625a109ca5290
Patrick Elfert <patrickelfert5@gmail.com>
2024-10-20 14:42:16 +0200

    use mask and svg to cutout shape for menu item

--- apps/immersion-player-ui/src/app/menu/menu.tsx
+++ apps/immersion-player-ui/src/app/menu/menu.tsx
@@ -14,26 +14,20 @@ function MenuItem({
 }) {
   return (
     <div
-      className={cn('flex flex-row h-[88px] w-full', {
-        'bg-primary-gradient': active,
-      })}
+      className={cn(
+        'cutout flex items-center justify-center w-[216px] h-[92px]',
+        { 'bg-primary-gradient': active, 'bg-plum cursor-pointer': !active }
+      )}
     >
-      <div className="flex-1 bg-plum">
-
-      </div>
-      <div className="flex-col w-[216px] flex">
-        <div className="bg-plum h-3 rounded-br-full"></div>
-        <div
-          className={cn('flex flex-row items-center flex-1', {
-            'text-white opacity-60': !active,
-          })}
-        >
-          <div className="rounded-full bg-plum h-max w-max p-2 ml-4 mr-3">
-            <img className="w-4 h-4" src={icon}/>
-          </div>
-          <label className="font-semibold">{label}</label>
+      <div
+        className={cn('flex flex-row items-center flex-1', {
+          'text-white opacity-60': !active,
+        })}
+      >
+        <div className="rounded-full bg-plum h-max w-max p-2 ml-4 mr-3">
+          <img className="w-4 h-4" src={icon} />
         </div>
-        <div className="bg-plum h-3 rounded-tr-full"></div>
+        <label className="font-semibold cursor-pointer">{label}</label>
       </div>
     </div>
   );
