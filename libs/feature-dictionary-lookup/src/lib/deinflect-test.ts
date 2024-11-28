737636ed925cc749c76ae35a56985e944a9caebb
Patrick Elfert <patrickelfert5@gmail.com>
2024-11-28 08:52:20 +0100

    add some more logging to failed tests

--- libs/feature-dictionary-lookup/src/lib/deinflect-test.ts
+++ libs/feature-dictionary-lookup/src/lib/deinflect-test.ts
@@ -1,5 +1,5 @@
 /// <reference types="jest" />
-import {getDeinflections} from "./deinflect";
+import { getDeinflections } from './deinflect';
 
 type DeinflectionTest = {
   input: string;
@@ -8,12 +8,23 @@ type DeinflectionTest = {
 };
 
 export function runTests(tests: DeinflectionTest[]) {
-  test.each(tests)("$description", ({ input, output }) => {
+  test.each(tests)('$description', ({ input, output }) => {
     /** the deinflection result can deliver more result which might be invalid,
      * this might not be a problem since we won't find them in a dictionary anyway **/
     const deinflected = getDeinflections(input);
-    expect(
-      output.every((expected) => deinflected.includes(expected)),
-    ).toBeTruthy();
+
+    const notFound = output.filter((expected) =>
+      !deinflected.includes(expected)
+    );
+
+    if (notFound.length > 0) {
+      console.error(
+        `Could not find ${notFound.join(',')} inside of ${deinflected.join(
+          ','
+        )}`
+      );
+    }
+
+    expect(notFound).toHaveLength(0);
   });
 }
