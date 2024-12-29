dd167e4d31a84696fa7cdc54d0a3ec9c2084e0d9
Patrick Elfert <patrickelfert5@gmail.com>
2024-12-29 18:35:16 +0100

    wip, display subtitles

--- libs/feature-media-player-ui/src/lib/feature-media-player-ui.tsx
+++ libs/feature-media-player-ui/src/lib/feature-media-player-ui.tsx
@@ -8,32 +8,30 @@ import useSubtitles from './hooks/useSubtitles';
 export function FeatureMediaPlayerUi() {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const libraryItem = useLibrary().find(
-    (entry) => entry.id === params.get('id')
-  );
-  const japaneseLanguage = libraryItem?.language.find(
-    (l) => l.languageCode === 'ja'
-  );
+  const libraryItem = useLibrary().find((entry) => entry.id === params.get('id'));
+  const japaneseLanguage = libraryItem?.language.find((l) => l.languageCode === 'ja');
   const { subtitles, isLoading } = useSubtitles(japaneseLanguage?.path);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   if (libraryItem) {
     return (
-      <div className="relative pt-[56.25%]">
+      <div className="relative">
         <video
           ref={videoRef}
           controls
+          controlsList={'nofullscreen'}
           src={libraryItem.path}
           height="100%"
           width="100%"
-          className={'absolute top-0 left-0'}
         ></video>
-        {
-          isLoading && <div className="absolute text-white bottom-[15%] right-1/2">Loading...</div>
-        }
+        {isLoading && <div className="absolute text-white bottom-[15%] right-1/2">Loading...</div>}
 
         {!isLoading && subtitles && (
-          <Subtitles className="absolute bottom-0 left-0" subtitles={subtitles} videoPlayerRef={videoRef} />
+          <Subtitles
+            className="absolute bottom-[15%] w-full flex justify-center"
+            subtitles={subtitles}
+            videoPlayerRef={videoRef}
+          />
         )}
       </div>
     );
--- libs/feature-media-player-ui/src/lib/subtitles/subtitles.tsx
+++ libs/feature-media-player-ui/src/lib/subtitles/subtitles.tsx
@@ -3,6 +3,11 @@ import { RefObject } from 'react';
 import { usePlayback } from '../hooks/usePlayback';
 import {cn} from "@immersion-player/shared-utils";
 
+function Dictionary() {
+  return <div className="h-80 bg-red-600">
+  </div>
+}
+
 export function Subtitles({
   subtitles,
   videoPlayerRef,
@@ -16,9 +21,9 @@ export function Subtitles({
 
   if (subtitles) {
     return (
-      <div className={cn('flex flex-row text-white', className)}>
+      <div className={cn('flex flex-row text-white text-2xl', className)}>
         {currentSubtitle?.tokens.map((token, index) => (
-          <div key={index}>{token.token}</div>
+          <div className={"hover:text-primary"} key={index}>{token.token}</div>
         ))}
       </div>
     );
