f8cb55d976b1e8e76ad767c9bdd1a444ec787093
Patrick Elfert <patrickelfert5@gmail.com>
2025-07-13 17:17:33 +0200

    debounce subtitle index, memoize browser row to avoid frequent rerenders

--- libs/feature-media-player-ui/src/lib/subtitles/browser.tsx
+++ libs/feature-media-player-ui/src/lib/subtitles/browser.tsx
@@ -1,39 +1,56 @@
-import { useEffect } from "react";
-import { cn, useIsScrolling } from "@immersion-player/shared-utils";
-import { SubtitleLine } from "./subtitles.js";
-import useSubtitles from "../hooks/useSubtitles.js";
+import { memo, useEffect, useRef } from 'react';
+import { cn, useIsScrolling } from '@immersion-player/shared-utils';
+import { SubtitleLine } from './subtitles.js';
+import useSubtitles from '../hooks/useSubtitles.js';
 import { useSubtitleStore } from '../state/subtitle.store.js';
+import { useDebounce, useRenderInfo } from '@uidotdev/usehooks';
+import { Subtitle } from '@immersion-player/shared-types';
 
 const formatTime = (timecode: string) => {
-    const [time] = timecode.split(',');
-    const [hours, minutes, seconds] = time.split(':').map(Number);
-    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  const [time] = timecode.split(',');
+  const [hours, minutes, seconds] = time.split(':').map(Number);
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
 };
 
 export function Browser() {
-    const currentSubtitleIndex = useSubtitleStore((state) => state.currentSubtitleIndex);
-    const { subtitles } = useSubtitles();
-    const {isScrolling, ref: containerRef} = useIsScrolling()
+  const currentSubtitleIndex = useSubtitleStore((state) => state.currentSubtitleIndex);
+  const debouncedSubtitleIndex = useDebounce(currentSubtitleIndex, 300);
+  const { subtitles } = useSubtitles();
+  //@Todo: Optimize performance of useIsScrolling
+  //const {isScrolling, ref: containerRef} = useIsScrolling()
+  const containerRef = useRef<HTMLDivElement>(null);
 
+  useRenderInfo('Browser');
 
-    useEffect(() => {
-        if (currentSubtitleIndex && containerRef.current) {
-            const container = containerRef.current;
-            const activeSubtitleElement = container.children[currentSubtitleIndex];
-            if (activeSubtitleElement) {
-                activeSubtitleElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
-            }
-        }
-    }, [currentSubtitleIndex, subtitles, containerRef]);
+  useEffect(() => {
+    if (debouncedSubtitleIndex && containerRef.current) {
+      const container = containerRef.current;
+      const activeSubtitleElement = container.children[debouncedSubtitleIndex];
+      if (activeSubtitleElement) {
+        activeSubtitleElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    }
+  }, [debouncedSubtitleIndex, subtitles, containerRef]);
 
-    return (<div ref={containerRef} className="rounded bg-content2 text-white overflow-y-scroll p-3">
-        {subtitles.map((subtitle, index) => {
-            return (<div key={index} className={cn('p-2 border-l-primary rounded', { 'border-l-4': currentSubtitleIndex === index })}>
-                <SubtitleLine disableDictionary={isScrolling} subtitle={subtitle} />
-                <small className="opacity-50">
-                    {formatTime(subtitle.startTime)} - {formatTime(subtitle.endTime)}
-                </small>
-            </div>)
-        })}
-    </div>)
+  return (
+    <div ref={containerRef} className="rounded bg-content2 text-white overflow-y-scroll p-3">
+      {subtitles.map((subtitle, index) => (
+        <BrowserRow subtitle={subtitle} index={index} isActive={currentSubtitleIndex === index}></BrowserRow>
+      ))}
+    </div>
+  );
 }
+
+const BrowserRow = memo(
+  ({ subtitle, index, isActive }: { subtitle: Subtitle; index: number; isActive: boolean }) => {
+    return (
+      <div key={index} className={cn('p-2 border-l-primary rounded', { 'border-l-4': isActive })}>
+        <SubtitleLine disableDictionary={false} subtitle={subtitle} />
+        <small className="opacity-50">
+          {formatTime(subtitle.startTime)} - {formatTime(subtitle.endTime)}
+        </small>
+      </div>
+    );
+  },
+  (prev, next) => prev.isActive === next.isActive && prev.subtitle.index === next.subtitle.index
+);
