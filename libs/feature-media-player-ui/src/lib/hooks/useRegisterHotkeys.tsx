a3776617abd8f8d80e95bd0630f5fffb83d43ca4
Patrick Elfert <patrickelfert5@gmail.com>
2025-07-14 16:22:06 +0200

    add space bar hotkey, add nudge to avoid getting stuck jumping to next subtitle

--- libs/feature-media-player-ui/src/lib/hooks/useRegisterHotkeys.tsx
+++ libs/feature-media-player-ui/src/lib/hooks/useRegisterHotkeys.tsx
@@ -1,25 +1,36 @@
 import { useHotkeys } from 'react-hotkeys-hook';
-import { RefObject } from 'react';
+import { RefObject} from 'react';
 import { useSubtitleStore } from '../state/subtitle.store.js';
 import { timecodeToSeconds } from '@immersion-player/shared-utils';
 import useSubtitles from './useSubtitles.js';
 
 export function useRegisterHotkeys(videoRef: RefObject<HTMLVideoElement>) {
-  const subtitleStore = useSubtitleStore();
   const {subtitles} = useSubtitles();
 
   function seekToSubtitle(offset: number) {
-    if (!videoRef.current || subtitleStore.currentSubtitleIndex == null) return;
+    const currentSubtitleIndex = useSubtitleStore.getState().currentSubtitleIndex;
+    if (!videoRef.current) return;
 
-    const targetIndex = subtitleStore.currentSubtitleIndex + offset;
+    const targetIndex = currentSubtitleIndex != null ? currentSubtitleIndex + offset : 0;
     if (targetIndex < 0 || targetIndex >= subtitles.length) return;
 
     const startTime = subtitles[targetIndex]?.startTime;
     if (!startTime) return;
 
-    videoRef.current.currentTime = timecodeToSeconds(startTime);
+    const nudge = 0.01
+    videoRef.current.currentTime = timecodeToSeconds(startTime) + nudge;
+  }
+
+  function togglePlay() {
+    if(!videoRef.current) return;
+    if(videoRef.current.paused) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
   }
 
   useHotkeys('left', () => seekToSubtitle(-1));
   useHotkeys('right', () => seekToSubtitle(1));
+  useHotkeys('space', () => togglePlay());
 }
