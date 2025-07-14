e1a824e2e552b7ae03aa7b37f261ca3858aa94ae
Patrick Elfert <patrickelfert5@gmail.com>
2025-07-14 16:21:01 +0200

    create hook to sync current subtitle to a store

---
+++ libs/feature-media-player-ui/src/lib/hooks/useSubtitleTracking.ts
@@ -0,0 +1,44 @@
+import { RefObject, useEffect } from 'react';
+import useSubtitles from './useSubtitles.js';
+import { Subtitle } from '@immersion-player/shared-types';
+import { timecodeToSeconds } from '@immersion-player/shared-utils';
+import { useSubtitleStore } from '../state/subtitle.store.js';
+
+export function useSubtitleTracking(videoRef: RefObject<HTMLVideoElement>) {
+  const { subtitles } = useSubtitles();
+  const resetSubtitleStore = useSubtitleStore((state) => state.reset);
+  const setCurrentSubtitle = useSubtitleStore((state) => state.setCurrentSubtitle);
+
+  useEffect(() => {
+    const currentPlayer = videoRef.current;
+
+    const handleTimeUpdate = () => {
+      const timestamp = videoRef.current?.currentTime ?? 0;
+      const subtitle = getCurrentSubtitle(subtitles, timestamp);
+
+      if (subtitle) {
+        setCurrentSubtitle(subtitle.value, subtitle.index);
+      }
+    };
+
+    if (currentPlayer) {
+      currentPlayer.addEventListener('timeupdate', handleTimeUpdate);
+    }
+    return () => {
+      resetSubtitleStore();
+      currentPlayer?.removeEventListener('timeupdate', handleTimeUpdate);
+    };
+  }, [videoRef, subtitles, setCurrentSubtitle, resetSubtitleStore]);
+}
+
+const getCurrentSubtitle = (subtitles: Subtitle[], timestamp: number) => {
+  const index = subtitles.findIndex((subtitle) => {
+    const start = timecodeToSeconds(subtitle.startTime);
+    const end = timecodeToSeconds(subtitle.endTime);
+    return start <= timestamp && timestamp <= end;
+  });
+
+  if (index === -1) return null;
+
+  return { value: subtitles[index], index };
+};
