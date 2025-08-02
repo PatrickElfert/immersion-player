e938cc526905f2a16f1145e2d8dbcb8ec82850d6
Patrick Elfert <patrickelfert5@gmail.com>
2025-08-02 13:20:35 +0200

    remove definitions from task implementation

--- apps/immersion-player-desktop/e2e/tasks/EnsureDefinitionsForWord.ts
+++ apps/immersion-player-desktop/e2e/tasks/EnsureDefinitionsForWord.ts
@@ -3,28 +3,18 @@ import { PlayerPage } from '../pages/player.page.js';
 import { Ensure, equals } from '@serenity-js/assertions';
 import { Hover } from '@serenity-js/web';
 
-export const ensureDefinitionsForWordInActiveSubtitles = (word: string, term: string) => {
+export const ensureDefinitionsForWordInActiveSubtitles = (
+  word: string,
+  definitions: {
+    text: string;
+    description: string;
+  }[]
+) => {
   return Task.where(
     `#actor ensures definitions for word "${word}" in current subtitles`,
     Wait.until(PlayerPage.currentSubtitle.isVisible(), equals(true)),
     Hover.over(PlayerPage.subtitleTokenWithText(word)),
     Wait.until(PlayerPage.dictionaryPanel.isVisible(), equals(true)),
-    Ensure.that(
-      PlayerPage.definitionsForDeinflectedTerm(word),
-      equals([
-        {
-          text: 'indicates sentence topic',
-          description: 'pronounced わ in modern Japanese',
-        },
-        {
-          text: 'indicates contrast with another option (stated or unstated)',
-          description: '',
-        },
-        {
-          text: 'adds emphasis',
-          description: '',
-        },
-      ])
-    )
+    Ensure.that(PlayerPage.definitionsForDeinflectedTerm(word), equals(definitions))
   );
 };
