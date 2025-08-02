de03406e9c20f5636a2f5d749fee7fddaaaad886
Patrick Elfert <patrickelfert5@gmail.com>
2025-08-02 14:04:14 +0200

    disable watch mode for vitetest to avoid blocking ci

--- libs/feature-dictionary-lookup/vite.config.ts
+++ libs/feature-dictionary-lookup/vite.config.ts
@@ -21,5 +21,6 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: { reportsDirectory: '../../coverage/libs/feature-dictionary-lookup', provider: 'v8' },
+    watch: false
   },
 });
--- libs/feature-media-player-ui/vite.config.ts
+++ libs/feature-media-player-ui/vite.config.ts
@@ -21,5 +21,6 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: { reportsDirectory: '../../coverage/libs/feature-media-player-ui', provider: 'v8' },
+    watch: false
   },
 });
